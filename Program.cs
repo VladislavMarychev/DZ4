225d2de22c6ea3e25ffd15cd8ba7ed32c7eac6a2
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-16 20:34:59 +0300

    первая домашка сделана

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+
+
+    Console.Write("Введите число A: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+    for (int i = 0; i < b; i++)
+    {
+        c = c * a;
+    }
+Console.WriteLine($"Число = {c} ");
