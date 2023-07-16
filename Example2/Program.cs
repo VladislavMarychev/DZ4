ad880527ca5f9b87cdb63bc21bff5c80f17eaffd
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-16 21:38:56 +0300

    второе задание готово

---
+++ Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int c = 0;
+    int b = 0;
+    while (a != 0)
+    {
+    c = a % 10;
+    b = b + c;
+    a = a / 10;
+    }
+Console.WriteLine(b);
